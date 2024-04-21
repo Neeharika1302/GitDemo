8067dd03596dcfd54542aee7d20de5a96909e875
Neeha1302 <pneeharika2000@gmail.com>
2024-04-21 17:48:22 +0530

    First Commit

---
+++ src/test/java/ExtentRepo/ExtentRepo.java
@@ -0,0 +1,34 @@
+package ExtentRepo;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class ExtentRepo {
+    ExtentReports extent;
+    @BeforeTest
+    public void config(){
+        String filePath=System.getProperty("user.dir")+"\\Reports\\index.html";
+        ExtentSparkReporter reporter=new ExtentSparkReporter(filePath);
+        reporter.config().setReportName("Web automation results");
+        reporter.config().setDocumentTitle("Test Results");
+        extent=new ExtentReports();
+        extent.attachReporter(reporter);
+        extent.setSystemInfo("Tester","Neeha");
+    }
+    @Test
+    public void sampleTest(){
+        ExtentTest test=extent.createTest("Sample Test");
+        WebDriver driver= new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://rahulshettyacademy.com/client/");
+        System.out.println(driver.getTitle());
+        test.fail("Results do not match");
+        driver.close();
+        extent.flush();
+    }
+}
